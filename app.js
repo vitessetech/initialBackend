b299d2b1417a3c6c7a17e8aa43127c58108780a0
Vitesse <arbaj897ansari@gmail.com>
2023-02-02 17:32:16 +0530

    added VerifiyToken method to app.js

--- app.js
+++ app.js
@@ -4,13 +4,20 @@ const express = require("express");
 const NotFoundMiddleware = require('./middlewares/not-found')
 const errorHandlerMiddleware = require('./middlewares/error-handler')
 const { createApp } = require("./crud-generator");
-
+const {VerifyToken} = require('./middlewares/auth')
 //Models For Generating through sequalize
 const user = require("./models/user");
 const role = require("./models/role");
 
 const sequelize = require("./utils/database");
+const { StatusCodes } = require("http-status-codes");
 const {app, createRoute } = createApp(sequelize);
+
+app.get('/',(__,res) => res.status(StatusCodes.ACCEPTED).json({success : true,msg : 'Crud Generator API'}))
+
+// app.use(VerifyToken)
+// use for authentication
+
 
 createRoute("roles", role, {
   onDelete: (req) =>
@@ -20,6 +27,7 @@ createRoute("roles", role, {
         console.log("res --", res).catch((err) => ("err --", err.response))
       ),
 });
+
 createRoute("users",user)
 
 
